9677e74ab493041f59a5cc5708eac7d317f6d9fe
manoeljunior <manoeljnr@gmail.com>
2015-02-11 12:41:41 -0300

    Inclusão do projeto do git.

---
+++ src/main/java/br/edu/fa7/exemploCucumber/AllTests.java
@@ -0,0 +1,12 @@
+package br.edu.fa7.exemploCucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions ()
+public class AllTests {
+
+}
